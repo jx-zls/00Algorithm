ad786e900b0b6ed577d47740c0c1c8ac2d342500
mapeng <18291820539@163.com>
2021-09-06 18:48:12 +0800

    计数、归并排序

--- algorithm/Sort/Sort/main.swift
+++ algorithm/Sort/Sort/main.swift
@@ -119,8 +119,104 @@ func selectSort() {
 //    }
 //}
 
-selectSort()
-print("选择 ==== \(in_datas)")
+//selectSort()
+//print("选择 ==== \(in_datas)")
+
+// 计数排序(桶排序)
+
+
+var count_datas = [1, 20, 8, 3, 10, 1, 7, 2]
+
+func countSort() {
+    
+    var max: Int = 0
+    var min: Int = count_datas.first!
+    for i in count_datas {
+        if min > i {
+            min = i
+        }
+        
+        if max < i {
+            max = i
+        }
+    }
+    
+    let count = max - min + 1
+    var allArr: [Int] = Array(repeating: 0, count: count)
+    var idx = 0
+    for i in count_datas {
+        allArr[i - min] += 1
+    }
+    print(allArr)
+    for (i, _) in allArr.enumerated() {
+        while allArr[i] > 0 {
+            count_datas[idx] = i + min
+            idx += 1
+            allArr[i] -= 1
+        }
+    }
+}
+
+//countSort()
+//print(count_datas)
+
+// 归并
+
+var merge_datas = [1, 20, 8, 3, 10, 1, 7, 2]
+
+func mergeSort(arr: [Int]) -> [Int] {
+   
+    print("mergeSort ==", arr)
+    if arr.count <= 1 {
+        return arr
+    }
+    let mid = arr.count / 2
+
+    let firstArr = mergeSort(arr: Array(arr[0 ..< mid]))
+    let secondArr = mergeSort(arr: Array(arr[mid ..< arr.count]))
+
+    let mergeData = merage(data1: firstArr, data2: secondArr)
+    return mergeData
+}
+
+func merage(data1: [Int], data2: [Int]) -> [Int] {
+    print(data1, data2)
+    var i = 0
+    var j = 0
+    var k = 0
+    let p = data1.count
+    let q = data2.count
+    var arr = Array(repeating: 0, count: p+q)
+    
+    while i < p, j < q {
+        if data1[i] <= data2[j] {
+            arr[k] = data1[i]
+            i += 1
+            k += 1
+        } else {
+            arr[k] = data2[j]
+            j += 1
+            k += 1
+        }
+    }
+    
+    while i < p {
+        arr[k] = data1[i]
+        i += 1
+        k += 1
+    }
+    
+    while j < q {
+        arr[k] = data2[j]
+        j += 1
+        k += 1
+    }
+    return arr
+}
+
+
+let mergedData = mergeSort(arr: merge_datas)
+print("========= ", mergedData)
 
 
 
